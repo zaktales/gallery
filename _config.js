e1f572562b9ecbf7a365cd0338b210cf32d4076c
zaktales <wizz@pop-os.localdomain>
2023-02-20 16:38:29 +0300

    config edited

--- _config.js
+++ _config.js
@@ -3,7 +3,5 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://zaktales:zaktales123@zcluster.lcnkuta.mongodb.net/?retryWrites=true&w=majority',
-    #development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    #test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
