d906eccb9c32e9888e123062bedc3e4483d4740f
zaktales <wizz@pop-os.localdomain>
2023-02-20 18:52:34 +0300

    Modified tests

--- _config.js
+++ _config.js
@@ -3,5 +3,6 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://zaktales:zaktales123@zcluster.lcnkuta.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://zaktales:zaktales123@zcluster.lcnkuta.mongodb.net/?retryWrites=true&w=majority'
 }
 module.exports = config;
